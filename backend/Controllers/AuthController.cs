5b5645e0fc787de9d9fdb0e5ae627158a1e51ab4
akshai <alphatech2909@gmail.com>
2023-06-28 10:31:57 +0530

    Add: JWT service method for token generation

---
+++ backend/Controllers/AuthController.cs
@@ -0,0 +1,61 @@
+﻿using backend.Data;
+using backend.TypeCheckingModel;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public IConfiguration? _configuration;
+        public ApplicationDbContext? _context;
+
+        public AuthController(IConfiguration? configuration, ApplicationDbContext? context)
+        {
+           this. _configuration = configuration;
+           this._context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            if(_context is not null)
+            {
+                try { 
+                   var TotalUsers = await _context.Authentications.ToListAsync();
+                   return Ok(JsonConvert.SerializeObject(TotalUsers)); 
+                }catch(Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+            }
+            return Ok("No Users Found");
+
+        }
+
+        public async Task<bool> UserExists(string user_id)
+        {
+            if(_context is not null)
+            {
+                try
+                {
+                    var user = await _context.Users.FindAsync(user_id);
+                    if(user is not null)
+                    {
+                        return true;
+                    }
+
+                }catch(Exception ex)
+                {
+                    return false;
+                }
+
+            }
+            return false;
+        }
+    }
+}
--- backend/Program.cs
+++ backend/Program.cs
@@ -1,7 +1,44 @@
+using backend.Data;
+using backend.Respository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using backend.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(o =>
+{
+    o.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = false,
+        ValidateIssuerSigningKey = true
+    };
+});
+builder.Services.AddAuthorization();
+// Add configuration from appsettings.json
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables();
 
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DB"))
+    );
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -17,8 +54,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
+IConfiguration configuration = app.Configuration;
+IWebHostEnvironment environment = app.Environment;
 
 app.MapControllers();
 
