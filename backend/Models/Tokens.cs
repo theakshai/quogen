01b0daed8d7b18648762c67305f814d5b9f3f373
akshai <akshaiakshai35@gmail.com>
2023-06-28 23:05:36 +0530

    Cleaning main branch

--- backend/Models/Tokens.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace backend.Models
-{
-    public class Tokens
-    {
-
-        public string? Token { get; set; }
-        public string? RefreshToken { get; set; }
-    }
-}
--- backend/docs/SqlQuery.sql
+++
@@ -1,4 +0,0 @@
-create Table Auth(
-	user_id varchar(50) primary key not null,
-	password varchar(50)
-	)
